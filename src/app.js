38389fc26f3e6e26b935a74f7a0fc8a20453c81a
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2023-06-23 14:24:55 -0300

    Refatorando

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import conexao from '../infra/conexao.js'
+import conexao from './app/database/conexao.js'
+import AlunoController from './app/controllers/AlunoController.js'
 
 const app = express()
 app.use(express.json()) // Indicar para o express ler body com JSON
@@ -16,80 +17,10 @@ function buscaIndexAluno(id) {
 }
 
 // Rotas
-app.get('/alunos', (req, res) => {
-    const sql = "SELECT * FROM alunos"
-    conexao.query(sql, (error, resultado ) => {
-        if (error){
-            res.status(404).json({'erro': error})
-        }
-        else {
-            res.status(200).json(resultado)
-        }
-    })
-})
-
-app.get('/alunos/:id', (req, res) => {
-    const id = req.params.id
-    const sql = "SELECT * FROM alunos WHERE id=?"
-
-    conexao.query(sql, id, (error, resultado ) => {
-        const linha = resultado[0] // A requisição sempre retorna um array porém de
-        //um elemento só, por isso é definido o index 0
-
-        if (error){
-            res.status(404).json({'erro': error})
-        }
-        else {
-            res.status(200).json(linha)
-        }
-    })
-})
-
-app.post('/alunos', (req, res) => {
-
-    const aluno = req.body
-    const sql = "INSERT INTO alunos SET ?"
-
-    conexao.query(sql, aluno, (error, resultado ) => {
-        if (error){
-            res.status(400).json({'erro': error})
-        }
-        else {
-            res.status(201).json(resultado)
-        }
-    })
-})
-
-app.delete('/alunos/:id', (req, res) => {
-    const id = req.params.id
-    const sql = "DELETE FROM alunos WHERE id=?"
-
-    conexao.query(sql, id, (error, resultado ) => {
-
-        if (error){
-            res.status(404).json({'erro': error})
-        }
-        else {
-            res.status(200).json(resultado)
-        }
-    })
-})
-
-app.put('/alunos/:id', (req, res) => {
-    // Criar validação caso o id não exista
-    const id = req.params.id
-    const aluno = req.body
-    const sql = "UPDATE alunos SET ? WHERE id=?"
-
-    conexao.query(sql, [aluno, id], (error, resultado ) => {
-        if (error){
-            res.status(400).json({'erro': error})
-        }
-        else {
-            res.status(201).json(resultado)
-        }
-    })
-})
-
+app.get('/alunos', AlunoController.index)
+app.get('/alunos/:id', AlunoController.show)
+app.post('/alunos', AlunoController.store)
+app.put('/alunos/:id', AlunoController.update)
+app.delete('/alunos/:id', AlunoController.delete)
 
 export default app //Exportar o app
