190a77f3af7ad718829ec0cba5851fc34dcb1883
kaliIinux <angelo.carnevale@hotmail.com>
2023-07-18 20:49:09 -0300

    Corrigindo erro de cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import express from 'express'
 import routes from './routes.js'
+import cors from 'cors'
 
 const app = express()
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 
 // Usar o router
 app.use(routes)
